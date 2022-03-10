7849734f8e3c1fccbcf58034cacc97e9742e39ae
Fran Tardencilla <frantardencilla@hotmail.com>
2022-03-09 21:25:12 -0500

    Create gameRoundFunction
    This functions determines wheather the user or the computer won
    the rock, paper, scissors game based on the comparisson of each
    option entered.

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("hello world")
+function computerPlay(){
+    /*We first create an array for all 3 different types of possible plays*/
+    const results = ["rock", "paper", "scissors"]
+    /* We get a random index, and extract the object from the array*/
+    let computerPlay = results[Math.floor(Math.random() * results.length)]
+    /*We return the value*/
+    return computerPlay
+}
+
+function gameRound(userElection, computerElection){
+
+    /*We first create a variable that holds all the possible values */
+    const gameOptions = ["rock","paper","scissors"]
+
+    /**We check if the 2 options are the same or if the user input options that are not valid */
+    if(userElection == computerElection){
+        return "it's a tie"
+    } else if (!gameOptions.includes(userElection) || !gameOptions.includes(computerElection)){
+        return "Please enter a valid option"
+    }
+
+    /**
+     * If everything is ok, we then get check if the computer won, if not it means the user won
+     */
+
+    const computerWins = (userElection == "rock" && computerElection == "paper") 
+    || (userElection == "paper" && computerElection == "scissors") 
+    || (userElection == "scissors" && computerElection == "rock")
+
+    /*Construct the message with the winner and why it won */
+    const result = computerWins ? `The computer wins ${computerElection} beats ${userElection} ` : `The user wins ${userElection} beats ${computerElection} `
+    return result
+    
+}
